55de998aabd8bb8f05dacae376a6cfa3e3377120
nikola <nikola.kujaca@icitapeng.us>
2014-01-27 12:31:14 +0100

    Test 2 Dodao image lbl, imageView i image btn

--- AndroidConnectingToPhpMySQL/gen/com/example/androidhive/R.java
+++ AndroidConnectingToPhpMySQL/gen/com/example/androidhive/R.java
@@ -14,16 +14,18 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int btnCreateProduct=0x7f050003;
-        public static final int btnDelete=0x7f050005;
-        public static final int btnSave=0x7f050004;
-        public static final int btnViewProducts=0x7f050008;
-        public static final int button1=0x7f050009;
-        public static final int inputDesc=0x7f050002;
-        public static final int inputName=0x7f050000;
-        public static final int inputPrice=0x7f050001;
-        public static final int name=0x7f050007;
-        public static final int pid=0x7f050006;
+        public static final int btnCreateProduct=0x7f050005;
+        public static final int btnDelete=0x7f050007;
+        public static final int btnSave=0x7f050006;
+        public static final int btnViewProducts=0x7f05000a;
+        public static final int button1=0x7f05000b;
+        public static final int button_selectpic=0x7f050001;
+        public static final int imageView_pic=0x7f050000;
+        public static final int inputDesc=0x7f050004;
+        public static final int inputName=0x7f050002;
+        public static final int inputPrice=0x7f050003;
+        public static final int name=0x7f050009;
+        public static final int pid=0x7f050008;
     }
     public static final class layout {
         public static final int add_product=0x7f030000;
@@ -34,6 +36,12 @@ public final class R {
     }
     public static final class string {
         public static final int app_name=0x7f040001;
+        public static final int description=0x7f040006;
         public static final int hello=0x7f040000;
+        public static final int image=0x7f040002;
+        public static final int price=0x7f040005;
+        public static final int product_name=0x7f040004;
+        public static final int selectImage=0x7f040003;
+        public static final int snimi=0x7f040007;
     }
 }
